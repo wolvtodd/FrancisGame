0d06e9b2a399c8a1da2fbc7a03dcc7fc2629c40c
Jovian Chen <wolvtodd@gmail.com>
2016-07-03 22:39:33 +0900

    room rotate test scene setup.

--- FrancisGame/Assets/Scripts/Room/RoomRotator.cs
+++ FrancisGame/Assets/Scripts/Room/RoomRotator.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class RoomRotator : MonoBehaviour
 {
-	[SerializeField] private Transform RotateTarget;
+	[SerializeField]
+	private Vector3 m_frontDirection = new Vector3(0.0f, 0.0f, -1.0f);
 
 	void Start()
 	{
@@ -12,8 +13,25 @@ public class RoomRotator : MonoBehaviour
 
 	void Update()
 	{
-
+		Debug.Log(m_frontDirection);
+		UpdateRotation();
 	}
 
+	void UpdateInput()
+	{
+		float deadZone = 0.1f;
+		if (Mathf.Abs(Input.GetAxis("Horizontal")) > deadZone)
+		{
 
+		}
+		if (Mathf.Abs(Input.GetAxis("Vertical")) > deadZone)
+		{
+
+		}
+	}
+
+	void UpdateRotation()
+	{
+		transform.rotation = Quaternion.LookRotation(m_frontDirection, Vector3.up);
+	}
 }
